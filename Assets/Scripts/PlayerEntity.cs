ef4b1dff1f0bd9f75dfbffce0d524afb9fa87dae
Nuker0s <nuker0s2@gmail.com>
2024-02-19 23:45:28 +0100

    unity physics import

--- Assets/Scripts/PlayerEntity.cs
+++ Assets/Scripts/PlayerEntity.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Entities;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class PlayerEntity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 100;
+    public class PlayerBaker : Baker<PlayerEntity>
     {
         
-    }
-    //test for commit
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public override void Bake(PlayerEntity authoring)
+        {
+            //throw new System.NotImplementedException();
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new Playerdata()
+            {
+                speed = authoring.speed,
+            });
+            Debug.Log(authoring.speed);
+            
+        }
     }
 }
